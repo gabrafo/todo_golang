d60be5bf88e0563a6a9be56f2fc8fd6677293d2b
Gabriel Fagundes <gafagundesmesquita@gmail.com>
2026-01-30 10:36:25 -0300

    feat: establish database connection and handle errors in main function

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,12 @@ import (
 	"syscall"
 
 	"github.com/gabrafo/todo_golang/internal/env"
+	"github.com/jackc/pgx/v5"
 )
 
 func main() {
+	ctx := context.Background()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 	slog.SetDefault(logger)
 
@@ -20,6 +23,14 @@ func main() {
 			dsn: env.MustEnv("DB_URL"),
 		},
 	}
+
+	conn, err := pgx.Connect(ctx, cfg.db.dsn)
+	if err != nil {
+		slog.Error("Unable to connect to database: ", "error", err)
+		os.Exit(1)
+	}
+	slog.Info("Connected to database")
+	defer conn.Close(ctx)
 
 	api := api{
 		config: cfg,
